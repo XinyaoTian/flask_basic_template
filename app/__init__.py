45a87d36cd1d17a036c2d7137dd5cd6929400e13
XinyaoTian <xinyaotian@XinyaoTians-MacBook-Pro.local>
2018-12-17 17:33:01 +0800

    Add 404&500 error page hander and blueprint register

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,38 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-from app import routes
+# db = SQLAlchemy()
+# migrate = Migrate()
+# login = LoginManager()
+# login.login_view = 'auth.login'
+# login.login_message = _l('Please log in to access this page.')
+# mail = Mail()
+# bootstrap = Bootstrap()
+# moment = Moment()
+# babel = Babel()
+
+# using factory model
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # db.init_app(app)
+    # migrate.init_app(app, db)
+    # login.init_app(app)
+    # mail.init_app(app)
+    # bootstrap.init_app(app)
+    # moment.init_app(app)
+    # babel.init_app(app)
+
+    # import errors blueprint
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    # import main blueprint
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,15 @@
 from flask import render_template, url_for
-from app import app
+from app.main import bp
+from flask import current_app
 
 
-@app.route('/')
-@app.route(url_for('index'), methods=['GET'])
+@bp.route('/')
+@bp.route('/index', methods=['GET'])
 def index():
     user = {'username': 'Jack'}
     posts = [
         {'author': 'Mike', 'body': 'Great to see you!'},
         {'author': 'Leon', 'body': 'Enjoy developing.'}
     ]
+    # This path indites the path in app/templates/
     return render_template('index.html', user=user, posts=posts)
